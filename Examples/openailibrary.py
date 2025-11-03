9dc628dacb4ac0d906e1bbc6e0f1594a0785a134
famseamus <famseamuss@gmail.com>
2025-11-02 18:13:09 -0800

    Update openailibrary.py

--- Examples/openailibrary.py
+++ Examples/openailibrary.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="", base_url="https://gpt.membean.lol")
+client = OpenAI(api_key="", base_url="https://gpt.membean.lol") # api key required but unnecessary for this api.
 
 resp = client.chat.completions.create(
     model="", # model param required in open ai library, unneeded.
